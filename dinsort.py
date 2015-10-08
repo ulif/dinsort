fceffa058b65e01114d2a948e04762223da8304b
ulif <uli@gnufix.de>
2015-10-08 14:53:25 +0200

    Add basic project files.

---
+++ dinsort.py
@@ -0,0 +1,1 @@
+# dinsort.py
---
+++ setup.py
@@ -0,0 +1,86 @@
+import os
+import sys
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+tests_path = os.path.join(os.path.dirname(__file__), 'tests')
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test"), ]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)                                # pragma: no cover
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+install_requires = [
+    'setuptools',
+    ]
+
+tests_require = [
+    'pytest >= 2.0.3',
+    'pytest-cov',
+    ]
+
+docs_require = [
+    'Sphinx',
+    ]
+
+setup(
+    name="dinsort",
+    version="0.1.dev0",
+    author="Uli Fouquet",
+    author_email="uli@gnufix.de",
+    description=(
+        "Sort terms according to German DIN."),
+    license="GPL 3.0",
+    keywords="sort terms order DIN 5007 german words",
+    url="https://github.com/ulif/dinsort/",
+    py_modules=['dinsort', ],
+    packages=[],
+    namespace_packages=[],
+    long_description=read('README.rst') + '\n\n\n' + read('CHANGES.rst'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Natural Language :: German",
+        "Topic :: Utilities",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Text Processing",
+        "Topic :: Internet :: WWW/HTTP :: Indexing/Search",
+        (
+            "License :: OSI Approved :: "
+            "GNU General Public License v3 or later (GPLv3+)"),
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+    tests_require=tests_require,
+    extras_require=dict(
+        tests=tests_require,
+        docs=docs_require,
+        ),
+    cmdclass={'test': PyTest},
+    entry_points={
+    },
+)
