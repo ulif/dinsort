d0b48bc9b8a47bb1f637c3b532b30e126d2f29a1
ulif <uli@gnufix.de>
2015-10-09 22:36:28 +0200

    Add another testcase for general dinsort items.

--- tests/test_dinsort.py
+++ tests/test_dinsort.py
@@ -1,7 +1,14 @@
 # -*- coding: utf-8 -*-
 #
 # tests for dinsort module.
-from dinsort import normalize
+from dinsort import normalize, VARIANT1, VARIANT2
+
+class TestDinsort(object):
+
+    def test_variants(self):
+        # we can get variants
+        assert VARIANT1 is not None
+        assert VARIANT2 is not None
 
 
 class TestNormalize(object):
