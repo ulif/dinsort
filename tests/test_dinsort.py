021addcb4e00edf7916a8d5bcfb00d0452740f9f
ulif <uli@gnufix.de>
2015-10-11 15:16:15 +0200

    Use unicode_literals for Python2.x in tests as well.

--- tests/test_dinsort.py
+++ tests/test_dinsort.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # tests for dinsort module.
+from __future__ import unicode_literals
 from dinsort import normalize, VARIANT1, VARIANT2
 
 class TestDinsort(object):
@@ -16,16 +17,16 @@ class TestNormalize(object):
 
     def test_normalize(self):
         # we can normalize strings
-        assert normalize(u"string") is not None
+        assert normalize("string") is not None
 
     def test_sharp_s(self):
         # sharp s equals 'ss'
-        assert normalize(u"ß") == u"ss"
+        assert normalize("ß") == "ss"
 
     def test_umlaut(self):
         # we get ä -> a by default (and variant1 explicitly requested)
-        assert normalize(u"ä") == u"a"
-        assert normalize(u"ä", variant=VARIANT1) == u"a"
+        assert normalize("ä") == "a"
+        assert normalize("ä", variant=VARIANT1) == "a"
 
     def test_umlaut_variant2(self):
         # we get ä -> ae with variant 2
