c7248096cdb25c25a32bf6de92e0c4cc4b7fa63b
ulif <uli@gnufix.de>
2015-10-13 10:53:37 +0200

    Officially support Python 3.4 as well.

--- setup.py
+++ setup.py
@@ -69,6 +69,8 @@ setup(
             "GNU General Public License v3 or later (GPLv3+)"),
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: Implementation :: CPython",
 
     ],
